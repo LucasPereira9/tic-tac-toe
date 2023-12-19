a9f97833544bb6f0482bb4ecfdc033bd9bbe138f
LucasPereira9 <lucas970997@gmail.com>
2023-12-19 13:59:39 -0300

    :sparkles: feat: theme and checkpoints made

---
+++ tic-tac-toe/src/app/global/theme.tsx
@@ -0,0 +1,12 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  colors: {
+    primary: '#F99779',
+    black: '#000',
+    easyWhite: '#D8D6D6',
+    white: '#fff',
+    gray: '#737373',
+    error: '#ff3333',
+    success: '#4BB543',
+  },
+};
--- tic-tac-toe/src/pages/Configurations/index.tsx
+++ tic-tac-toe/src/pages/Configurations/index.tsx
@@ -2,13 +2,16 @@
 
 import { useEffect } from "react";
 import { useGlobalContext } from "../../app/Context/index"
+import theme from "../../app/global/theme";
 
 
 export default function Configuration() {
+  
   const {userAge, userName, setUserAge, setUserName} = useGlobalContext()
 
   useEffect(() => {
     console.log('user: ', userAge, userName)
+    console.log(theme)
   },[userAge, userName])
   return (
     <main>
