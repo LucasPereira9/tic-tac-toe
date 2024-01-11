c012650540d8ac0cd2983daa33a81566c9736176
LucasPereira9 <lucas970997@gmail.com>
2024-01-11 14:55:21 -0300

    :sparkles: feat: sound effects added

--- tic-tac-toe/src/app/components/primaryButton/index.tsx
+++ tic-tac-toe/src/app/components/primaryButton/index.tsx
@@ -5,12 +5,22 @@ import { IPrimaryButtonProps } from './primaryButton.structure';
 const AnimatedButton = (props: IPrimaryButtonProps) => {
   const [isHovered, setIsHovered] = useState(false);
 
+  const playClickSound = () => {
+    const clickSound = new Audio('/assets/soundEffects/buttonPressed.mp3');
+    clickSound.play();
+  };
+
+  const handleButtonClick = () => {
+    playClickSound();
+    props.onClick();
+  };
+
   return (
     <div
       className={styles.buttonWrapper}
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
-      onClick={props.onClick}
+      onClick={handleButtonClick}
     >
       <span className={styles.buttonText}>{props.title}</span>
       <div className={`${styles.animationLine} ${isHovered ? styles.active : ''}`}></div>
